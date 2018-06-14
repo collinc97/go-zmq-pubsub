0caf23665153c4e2f653fb416cc50b7c7407e576
Collin Chin <collin.chin@berkeley.edu>
2018-06-13 23:43:06 -0400

    naming

--- client3.go
+++ client3.go
@@ -1,84 +1,35 @@
 //
-//  Clone server Model Three
+//  Freelance client - Model 3.
+//  Uses flcliapi class to encapsulate Freelance pattern
 //
 
 package main
 
 import (
-	zmq "github.com/pebbe/zmq4"
-	"github.com/pebbe/zmq4/examples/kvsimple"
+	"github.com/pebbe/zmq4/examples/flcliapi"
 
 	"fmt"
 	"time"
 )
 
 func main() {
-	snapshot, _ := zmq.NewSocket(zmq.ROUTER)
-	snapshot.Bind("tcp://*:5556")
-	publisher, _ := zmq.NewSocket(zmq.PUB)
-	publisher.Bind("tcp://*:5557")
-	collector, _ := zmq.NewSocket(zmq.PULL)
-	collector.Bind("tcp://*:5558")
+	//  Create new freelance client object
+	client := flcliapi.New()
 
-	//  The body of the main task collects updates from clients and
-	//  publishes them back out to clients:
+	//  Connect to several endpoints
+	client.Connect("tcp://localhost:5555")
+	client.Connect("tcp://localhost:5556")
+	client.Connect("tcp://localhost:5557")
 
-	sequence := int64(0)
-	kvmap := make(map[string]*kvsimple.Kvmsg)
-
-	poller := zmq.NewPoller()
-	poller.Add(collector, zmq.POLLIN)
-	poller.Add(snapshot, zmq.POLLIN)
-LOOP:
-	for {
-		polled, err := poller.Poll(1000 * time.Millisecond)
+	//  Send a bunch of name resolution 'requests', measure time
+	start := time.Now()
+	req := []string{"random name"}
+	for requests := 1000; requests > 0; requests-- {
+		_, err := client.Request(req)
 		if err != nil {
+			fmt.Println("E: name service not available, aborting")
 			break
 		}
-		for _, item := range polled {
-			switch socket := item.Socket; socket {
-			case collector:
-				//  Apply state update sent from client
-				kvmsg, err := kvsimple.RecvKvmsg(collector)
-				if err != nil {
-					break LOOP //  Interrupted
-				}
-				sequence++
-				kvmsg.SetSequence(sequence)
-				kvmsg.Send(publisher)
-				kvmsg.Store(kvmap)
-				fmt.Println("I: publishing update", sequence)
-			case snapshot:
-				//  Execute state snapshot request
-				msg, err := snapshot.RecvMessage(0)
-				if err != nil {
-					break LOOP
-				}
-				identity := msg[0]
-
-				//  Request is in second frame of message
-				request := msg[1]
-				if request != "ICANHAZ?" {
-					fmt.Println("E: bad request, aborting")
-					break LOOP
-				}
-				//  Send state snapshot to client
-
-				//  For each entry in kvmap, send kvmsg to client
-				for _, kvmsg := range kvmap {
-					snapshot.Send(identity, zmq.SNDMORE)
-					kvmsg.Send(snapshot)
-				}
-
-				//  Now send END message with sequence number
-				fmt.Println("I: sending shapshot =", sequence)
-				snapshot.Send(identity, zmq.SNDMORE)
-				kvmsg := kvsimple.NewKvmsg(sequence)
-				kvmsg.SetKey("KTHXBAI")
-				kvmsg.SetBody("")
-				kvmsg.Send(snapshot)
-			}
-		}
 	}
-	fmt.Printf("Interrupted\n%d messages handled\n", sequence)
+	fmt.Println("Average round trip cost:", time.Now().Sub(start)/1000)
 }
